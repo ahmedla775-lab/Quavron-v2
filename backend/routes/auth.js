1a07edaa803702edc43c9873769c193dbb276d2a
sponsnordj <nwraldynbnywsf67@gmail.com>
2026-06-29 22:38:43 +0100

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,44 @@
+const express = require("express");
 
+const router = express.Router();
+
+/* ========================================
+Login
+======================================== */
+
+router.post("/login", (req, res) => {
+
+res.json({
+success: true,
+message: "Login API Ready"
+});
+
+});
+
+/* ========================================
+Register
+======================================== */
+
+router.post("/register", (req, res) => {
+
+res.json({
+success: true,
+message: "Register API Ready"
+});
+
+});
+
+/* ========================================
+Logout
+======================================== */
+
+router.post("/logout", (req, res) => {
+
+res.json({
+success: true,
+message: "Logout API Ready"
+});
+
+});
+
+module.exports = router;
