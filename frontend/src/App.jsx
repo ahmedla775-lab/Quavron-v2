17d60b0df35abe2d858584dad8a5e1bc3523547e
sponsnordj <nwraldynbnywsf67@gmail.com>
2026-06-30 13:04:41 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+
+return (
+
+<div className="container">
+
+  <h1>🚀 Quavron</h1>
+
+  <p>
+
+    Next Generation Cloud IDE Platform
+
+  </p>
+
+</div>
+
+);
+
+}
+
+export default App;
